890b8d442aa4d68224f4cf7599e6fb82d1989e5d
saadsiddiqui007 <156573463+saadsiddiqui007@users.noreply.github.com>
2024-01-21 20:16:09 +0500

    Add files via upload

---
+++ assignmrnt.js
@@ -0,0 +1,23 @@
+//Assignment 1
+let age;
+let isStudent;
+age = 16;
+isStudent = true;
+console.log("Age: ", age);
+console.log("Is Student: ", isStudent);
+
+window.onload = function() {
+    alert('Welcome to my Portfolio! I am glad to have you here.');
+   };
+
+
+//Assignment 2
+let length = 10;
+let width = 5;
+
+let area = length * width;
+
+console.log("The area of the rectangle is: " + area);
+let userName = prompt("Please enter your name:");
+let greeting = "Hello, " + userName + "! Welcome to our website!";
+alert(greeting);
